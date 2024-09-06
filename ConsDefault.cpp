e392fd544624414e6e86d5edb6ff63b4f4f32ae6
Mansi-082 <mansi.kulkarni.btech2023@sitpune.edu.in>
2024-09-06 10:33:45 +0530

    Add files via upload

---
+++ ConsDefault.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class Mansi
+{
+    public:
+    Mansi() {
+        cout<<"Hello Mansi"<<endl;
+    }
+};
+int main() {
+    Mansi obj; 
+    return 0;
+}
